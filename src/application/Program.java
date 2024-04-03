c498d37949ac90def150d625457fbefd878c5705
apsaires <andre.p.aires@gmail.com>
2024-04-03 19:31:21 +0100

    six

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -13,26 +13,39 @@ public class Program {
 
 		Connection conn = null;
 		Statement st = null;
-		ResultSet rs = null;
 		try {
 			conn = DB.getConnection();
 	
+			conn.setAutoCommit(false);
+
 			st = conn.createStatement();
 			
-			rs = st.executeQuery("select * from department");
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+			//int x = 1;
+			//if (x < 2) {
+			//	throw new SQLException("Fake error");
+			//}
 			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-			}
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("rows1 = " + rows1);
+			System.out.println("rows2 = " + rows2);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
-		}
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+		} 
 		finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
 	}
 }
